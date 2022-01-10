55bd3ff2fc9296d3eab91366d172dfe9e4056044
brettmortensen <11483419+brettmortensen@users.noreply.github.com>
2022-01-10 15:05:05 -0700

    Publish Java library with GitHub Action
    Adds GitHub Action to automate the deploying of the library to Maven on
    changes to the master branch. Utilizes the `setup-java` action as well
    as the `nexus-staging-maven-plugin`.

--- src/main/java/com/mx/client/ApiClient.java
+++ src/main/java/com/mx/client/ApiClient.java
@@ -125,7 +125,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("OpenAPI-Generator/0.4.0/java");
+        setUserAgent("OpenAPI-Generator/0.4.1/java");
 
         authentications = new HashMap<String, Authentication>();
     }
