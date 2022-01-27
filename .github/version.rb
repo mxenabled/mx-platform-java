340a0013c7a4d55fb6d7cb03e4994e82a0bef680
brettmortensen <11483419+brettmortensen@users.noreply.github.com>
2022-01-27 10:11:59 -0700

    Add version select
    Adds the ability to choose a semantic version (major, minor, patch) when
    updating the library.
    
    Also adds the `openapitools.json` file to the allowlist as recommended
    by the `openapi-generator-cli` README.

--- .github/version.rb
+++ .github/version.rb
@@ -1,3 +1,19 @@
 require "yaml"
-config = ::YAML.load(File.read("openapi/config.yml"))
+config = ::YAML.load(::File.read("openapi/config.yml"))
+major, minor, patch = config["artifactVersion"].split(".")
+
+case ARGV[0]
+when "major"
+  major = major.succ
+  minor = 0
+  patch = 0
+when "minor"
+  minor = minor.succ
+  patch = 0
+when "patch"
+  patch = patch.succ
+end
+
+config["artifactVersion"] = "#{major}.#{minor}.#{patch}"
+::File.open("openapi/config.yml", 'w') { |file| ::YAML.dump(config, file) }
 puts config["artifactVersion"]
