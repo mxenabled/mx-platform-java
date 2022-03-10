dcb55b485db75d18b8b13676696646e9e465f214
devexperience <devexperience@mx.com>
2022-03-10 17:12:51 +0000

    Generated version 0.8.2
    This pull request was automatically generated by a GitHub Action to generate version 0.8.2 of this library.

--- src/main/java/com/mx/client/ApiClient.java
+++ src/main/java/com/mx/client/ApiClient.java
@@ -133,7 +133,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("OpenAPI-Generator/0.8.1/java");
+        setUserAgent("OpenAPI-Generator/0.8.2/java");
 
         authentications = new HashMap<String, Authentication>();
     }
