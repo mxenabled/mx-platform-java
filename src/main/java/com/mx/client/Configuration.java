6a2148390457c2806ff573ae7f049d81d4c84344
devexperience <devexperience@mx.com>
2025-08-26 17:21:59 +0000

    Generated version 1.8.0
    This commit was automatically created by a GitHub Action to generate version 1.8.0 of this library.

--- src/main/java/com/mx/client/Configuration.java
+++ src/main/java/com/mx/client/Configuration.java
@@ -15,7 +15,7 @@ package com.mx.client;
 
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen")
 public class Configuration {
-    public static final String VERSION = "1.7.0";
+    public static final String VERSION = "1.8.0";
 
     private static ApiClient defaultApiClient = new ApiClient();
 
