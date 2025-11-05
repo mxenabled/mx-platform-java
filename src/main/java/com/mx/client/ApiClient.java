8a17e7920ffe5c4c0c4cf720407aeca167c2d5b9
devexperience <devexperience@mx.com>
2025-11-05 20:39:55 +0000

    Generated version 1.9.0
    This commit was automatically created by a GitHub Action to generate version 1.9.0 of this library.

--- src/main/java/com/mx/client/ApiClient.java
+++ src/main/java/com/mx/client/ApiClient.java
@@ -145,7 +145,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("OpenAPI-Generator/1.8.0/java");
+        setUserAgent("OpenAPI-Generator/1.9.0/java");
 
         authentications = new HashMap<String, Authentication>();
     }
